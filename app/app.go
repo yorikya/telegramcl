928892ea67f2b77dacf27853f7d82f1f90c0efac
Yuri <yuri@appsflyer.com>
2020-03-30 09:22:13 +0300

    Coder reordering

--- app/app.go
+++ app/app.go
@@ -96,7 +96,7 @@ func New(users, onBoardUsers, orders *cache.Cache, bot *tbot.Server, manager str
 		botServer:    bot,
 		bot:          bot.Client(),
 		manager:      manager,
-		managerKey:   "--manager",
+		managerKey:   "--manager",  //Telegram username not accept '--' chars, the manager key can't be oviriten with another username
 	}
 
 	bot.Use(a.messageListener) // add messageListener middleware to bot
@@ -123,15 +123,16 @@ func (a *Application) validateRegestredUser(username string) bool {
 	return found
 }
 
-func (a *Application) sendMessage(chatID string, text string) {
+func (a *Application) sendMessage(chatID, username,  text string) {
 	if _, err := a.bot.SendMessage(chatID, text); err != nil {
-		log.Printf("get an erro when send a message:'%s', ChatID: %s, error: %s", chatID, text, err)
+		log.Printf("get an erro when send a message: %s to user: %s, ChatID: %s, error: %s", text, username, chatID, err)
 	}
+	log.Printf("send message to user: %s, message: %s", username, text)
 }
 
 func (a *Application) sendMessageToManger(msg string) {
 	if a.managerChatID != "" {
-		a.sendMessage(a.managerChatID, msg)
+		a.sendMessage(a.managerChatID, "--Manager", msg)
 		return
 	}
 	log.Println("Not has a working manager, discard message", msg)
@@ -151,13 +152,11 @@ func (a *Application) completeOrder(m *tbot.Message) {
 		order := o.(*Order)
 		order.Items = append(order.Items, m.Text)
 		
-		log.Printf("user %s add ietms %s to t order", m.Chat.Username,m.Text)
-		a.sendMessage(m.Chat.ID, "items added, to finish order /end")
+		a.sendMessage(m.Chat.ID, m.Chat.Username, "items added, to finish order /end")
 		return
 	}
 	
-	log.Printf("user %s dos not have opened order", m.Chat.Username)
-	a.sendMessage(m.Chat.ID, "complete order, somthing went wrong")
+	a.sendMessage(m.Chat.ID, m.Chat.Username, "complete order, somthing went wrong")
 }
 
 func (a *Application) onBoardingProcess(username string) bool {
@@ -175,12 +174,10 @@ func (a *Application) startOnBoarding(username, chatID string) {
 	a.onBoradUsers.Set(username, &User{
 		Username: username,
 	}, cache.DefaultExpiration)
-	log.Printf("start user registration '%s'", username)
-	a.sendMessage(chatID, "please enter phone number (ex: 0521234567)")
+	a.sendMessage(chatID, username,"please enter phone number (ex: 0521234567)")
 }
 
 func (a *Application) completeOnboarding(m *tbot.Message) {
-	log.Printf("handle onboarding for '%s'", m.Chat.Username)
 	u, found := a.onBoradUsers.Get(m.Chat.Username)
 	if !found {
 		log.Println("error onboarding, can not find user '%s'", m.Chat.Username)
@@ -191,32 +188,26 @@ func (a *Application) completeOnboarding(m *tbot.Message) {
 	switch user.OnboardingState {
 	case phoneRequire:
 		user.Phone = m.Text //TODO: Validate phone number
-		log.Printf("user %s update phone number: %s", m.Chat.Username, m.Text)
 		user.OnboardingState = addressRequire
-		a.sendMessage(m.Chat.ID, "please enter adress (ex: Zavitan 9)")
+		a.sendMessage(m.Chat.ID, m.Chat.Username,"please enter adress (ex: Zavitan 9)")
 	case addressRequire:
 		user.Address = m.Text
-		log.Printf("user %s update address: %s", m.Chat.Username, m.Text)
 		user.OnboardingState = cityRequire
-		a.sendMessage(m.Chat.ID, "please enter city (ex: Kazerin)")
+		a.sendMessage(m.Chat.ID, m.Chat.Username,"please enter city (ex: Kazerin)")
 	case cityRequire:
 		user.City = m.Text
-		log.Printf("user %s update city: %s", m.Chat.Username, m.Text)
 		user.OnboardingState = descriptionRequire
-		a.sendMessage(m.Chat.ID, "please enter descritpion for courier (ex: House behind the post office wiht big white gate)")
+		a.sendMessage(m.Chat.ID, m.Chat.Username, "please enter descritpion for courier (ex: House behind the post office wiht big white gate)")
 	case descriptionRequire:
 		user.Description = m.Text
-		log.Printf("user %s update description for courier: %s", m.Chat.Username, m.Text)
 		user.OnboardingState = onboardingComplete
 
 		a.users.Set(m.Chat.Username, user, cache.DefaultExpiration)
-		log.Printf("create a new user: %+v", user)
 
 		a.onBoradUsers.Delete(m.Chat.Username) //TODO: create API for delete onboarding users (using for stucking users)
-		log.Printf("delete onboarding user %s", m.Chat.Username)
 
 		a.sendMessageToManger(fmt.Sprintf("craete new user %+v", user))
-		a.sendMessage(m.Chat.ID, fmt.Sprintf("on boarding complete! now you can try your first delivery"))
+		a.sendMessage(m.Chat.ID, m.Chat.Username, fmt.Sprintf("on boarding complete! now you can try your first delivery"))
 		a.sendMainMenu(m.Chat.ID)
 	default:
 		log.Printf("unknown user state, user: '%s', state: %d", m.Chat.Username, user.OnboardingState)
@@ -225,7 +216,7 @@ func (a *Application) completeOnboarding(m *tbot.Message) {
 
 func (a *Application) startHandler(m *tbot.Message) {
 	if a.validateUserInfo(m.Chat) {
-		a.sendMessage(m.Chat.ID, "please define @Username in you telegram app")
+		a.sendMessage(m.Chat.ID, "<nil>","please define @Username in you telegram app")
 		return
 	}
 
@@ -244,19 +235,16 @@ func (a *Application) endHandler(m *tbot.Message) {
 			order.Complete = true
 			order.EndTime = time.Now()
 			a.orders.Delete(m.Chat.Username)
-			a.sendMessage(m.Chat.ID, "order completed")
+			a.sendMessage(m.Chat.ID, m.Chat.Username, "order completed")
 			a.sendMainMenu(m.Chat.ID)
 			
 			a.sendMessageToManger(fmt.Sprintf("get an new order:\n %+v", *order)) //TODO: Send order email
-			log.Printf("user %s completed order", m.Chat.Username)
 			return
 		}
-		log.Printf("user %s does not have an open order", m.Chat.Username)
-		a.sendMessage(m.Chat.ID, "you dont have an open order")
+		a.sendMessage(m.Chat.ID, m.Chat.Username, "you dont have an open order")
 		return
 	}
-	log.Printf("end order for user %s get an error user missing", m.Chat.ID)
-	a.sendMessage(m.Chat.ID, "end order error")
+	a.sendMessage(m.Chat.ID, m.Chat.Username, "end order error")
 }
 
 func (a *Application) startWorkHandler(m *tbot.Message) {
@@ -267,22 +255,20 @@ func (a *Application) startWorkHandler(m *tbot.Message) {
 			OnboardingState: onboardingComplete,
 		}, cache.DefaultExpiration)
 		a.managerChatID = m.Chat.ID
-		log.Printf("manager %s start working, chatID: %s", a.manager, a.managerChatID)
-		a.sendMessage(m.Chat.ID, "start working")
+		a.sendMessageToManger("start working, to finish work /finish")
 		return
 	}
-	log.Printf("user %s try to start working", m.Chat.Username)
-	a.sendMessage(m.Chat.ID, "you are not authorize")
+	a.sendMessage(m.Chat.ID, m.Chat.Username, "you are not authorize")
 }
 
 func (a *Application) stopWorkHandler(m *tbot.Message) {
 	if m.Chat.Username == a.manager {
 		a.users.Delete(a.managerKey)
-		log.Printf("manager %s is finish work", a.manager)
-		a.sendMessage(m.Chat.ID, "finish working")
+		a.sendMessage(m.Chat.ID, m.Chat.Username, "finish working")
+		return
 	}
-	log.Printf("user %s try to stop working", m.Chat.Username)
-	a.sendMessage(m.Chat.ID, "you are not authorize")
+	
+	a.sendMessage(m.Chat.ID, m.Chat.Username, "you are not authorize")
 }
 
 func (a *Application) callbackHandler(cq *tbot.CallbackQuery) {
@@ -294,37 +280,33 @@ func (a *Application) callbackHandler(cq *tbot.CallbackQuery) {
 				StartTime: time.Now(),
 				OrderType: cq.Data,
 			}, cache.DefaultExpiration)
-			a.sendMessage(cq.Message.Chat.ID, `please send items list with new line seperator
+			a.sendMessage(cq.Message.Chat.ID, cq.Message.Chat.Username, `please send items list with new line seperator
 Example:
 Milk 3% - 1
 Elite Black Coffe - 4`)
 			return
 		}
-		log.Printf("new order was failed, user %s not found", cq.Message.Chat.Username)
+		a.sendMessage(cq.Message.Chat.ID, cq.Message.Chat.Username, "somthing went wrong")
 	case "new_delivery":
 		if !a.validateRegestredUser(cq.Message.Chat.Username) {
-			log.Printf("user %s failed to create new new order, user not found")
-			a.sendMessage(cq.Message.Chat.ID, "somthing went wrong")
+			a.sendMessage(cq.Message.Chat.ID, cq.Message.Chat.Username, "somthing went wrong")
 			return
 		}
 
 		if !a.canOrder(cq.Message.Chat.Username) {
-			a.sendMessage(cq.Message.Chat.ID, "delivery unavaible now")
+			a.sendMessage(cq.Message.Chat.ID, cq.Message.Chat.Username, "delivery unavaible now")
 			return
 		} 
 
-		if _, err := a.bot.SendMessage(cq.Message.Chat.ID, "Start new delivery", tbot.OptInlineKeyboardMarkup(deliveryTypeButtons())); err != nil {
+		if _, err := a.bot.SendMessage(cq.Message.Chat.ID, "New delivery", tbot.OptInlineKeyboardMarkup(deliveryTypeButtons())); err != nil {
 			log.Println("get an error when send new order message")
 		}
 
 	case "delivery_info":
-		a.bot.AnswerCallbackQuery(cq.ID, tbot.OptText("Delivery info"))
-		_, _ = a.bot.SendMessage(cq.Message.Chat.ID, `קיימת הזמנה 1
+		a.sendMessage(cq.Message.Chat.ID, cq.Message.Chat.Username,`קיימת הזמנה 1
 🟢 בטיפול
 ⏱ זמן הגעה משואר 10:20`)
-
 	}
-
 }
 
 func (a *Application) messageListener(h tbot.UpdateHandler) tbot.UpdateHandler {
@@ -335,8 +317,7 @@ func (a *Application) messageListener(h tbot.UpdateHandler) tbot.UpdateHandler {
 				a.completeOnboarding(u.Message)
 				return
 			case (u.Message.Chat.Username != a.manager) && !a.validateRegestredUser(u.Message.Chat.Username) && (u.Message.Text != "/start"):
-				a.sendMessage(u.Message.Chat.ID, "you need complete registration /start")
-				fmt.Printf("user %s require complete registartion", u.Message.Chat.Username)
+				a.sendMessage(u.Message.Chat.ID, u.Message.Chat.Username, "you need complete registration /start")
 				return
 			case a.onOrderProcess(u.Message.Chat.Username) && (u.Message.Text != "/end"):
 				a.completeOrder(u.Message)
